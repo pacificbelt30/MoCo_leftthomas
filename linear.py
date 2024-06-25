c6bafa79bdb64bd63b7c2481c377ae230576c8a8
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2024-06-25 17:13:16 +0900

    wandb setting for linear.py

--- linear.py
+++ linear.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from torchvision.datasets import CIFAR10
+from torchvision.datasets import CIFAR10, STL10
 from tqdm import tqdm
 
 import utils
@@ -65,12 +65,39 @@ if __name__ == '__main__':
                         help='The pretrained model path')
     parser.add_argument('--batch_size', type=int, default=256, help='Number of images in each mini-batch')
     parser.add_argument('--epochs', type=int, default=100, help='Number of sweeps over the dataset to train')
+    parser.add_argument('--lr', default=1e-3, type=float, help='Learning Rate at the training start')
+    parser.add_argument('--weight_decay', default=1e-6, type=float, help='Weight Decay')
+    parser.add_argument('--dataset', default='stl10', type=str, help='Training Dataset (e.g. CIFAR10, STL10)')
+    parser.add_argument('--wandb_model_runpath', default='', type=str, help='the runpath if using a model stored in WandB')
+    parser.add_argument('--wandb_project', default='default_project', type=str, help='WandB Project name')
+    parser.add_argument('--wandb_run', default='default_run', type=str, help='WandB run name')
 
     args = parser.parse_args()
     model_path, batch_size, epochs = args.model_path, args.batch_size, args.epochs
-    train_data = CIFAR10(root='data', train=True, transform=utils.train_transform, download=True)
+    lr, weight_decay = args.lr, args.weight_decay
+
+    if args.wandb_model_runpath != '':
+        base_model = wandb.restore(args.model_path, run_path=args.wandb_model_runpath)
+        model_path = base_model.name
+
+    # wandb init
+    config = {
+        "lr": args.lr,
+        "weight_decay": args.weight_decay,
+        "arch": "resnet34",
+        "dataset": args.dataset,
+        "epochs": args.epochs,
+        "batch_size": args.batch_size,
+        "model": model_path
+        "wandb_model_runpath": arsg.wandb_model_runpath
+    }
+    wandb.init(project=args.wandb_project, name=args.wandb_run, config=config)
+
+    # train_data = CIFAR10(root='data', train=True, transform=utils.train_transform, download=True)
+    train_data = STL10(root='data', split='train', transform=utils.train_transform, download=True)
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=16, pin_memory=True)
-    test_data = CIFAR10(root='data', train=False, transform=utils.test_transform, download=True)
+    # test_data = CIFAR10(root='data', train=False, transform=utils.test_transform, download=True)
+    train_data = STL10(root='data', split='test', transform=utils.train_transform, download=True)
     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=16, pin_memory=True)
 
     model = Net(num_class=len(train_data.classes), pretrained_path=model_path).cuda()
@@ -89,12 +116,17 @@ if __name__ == '__main__':
         results['train_acc@1'].append(train_acc_1)
         results['train_acc@5'].append(train_acc_5)
         test_loss, test_acc_1, test_acc_5 = train_val(model, test_loader, None)
+        wandb.log({'train_loss': train_loss, 'train_acc@1': train_acc_1, 'train_acc@5': train_acc_5})
         results['test_loss'].append(test_loss)
         results['test_acc@1'].append(test_acc_1)
         results['test_acc@5'].append(test_acc_5)
+        wandb.log({'test_loss': test_loss, 'test_acc@1': test_acc_1, 'test_acc@5': test_acc_5})
         # save statistics
         data_frame = pd.DataFrame(data=results, index=range(1, epoch + 1))
         data_frame.to_csv('results/linear_statistics.csv', index_label='epoch')
         if test_acc_1 > best_acc:
             best_acc = test_acc_1
             torch.save(model.state_dict(), 'results/linear_model.pth')
+
+    wandb.save('results/linear_model.pth'.format(save_name_pre))
+    wandb.finish()
