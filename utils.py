9fe72c6e6ef5a9fffd1188f40f2d924cac1d3a8f
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2024-06-27 15:31:49 +0900

    cifar100 added

--- utils.py
+++ utils.py
@@ -21,6 +21,23 @@ class CIFAR10Pair(datasets.CIFAR10):
 
         return pos_1, pos_2, target
 
+class CIFAR100Pair(datasets.CIFAR100):
+    """CIFAR100 Dataset.
+    """
+
+    def __getitem__(self, index):
+        img, target = self.data[index], self.targets[index]
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            pos_1 = self.transform(img)
+            pos_2 = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return pos_1, pos_2, target
+
 class STL10Pair(datasets.STL10):
     """STL10 Dataset.
     """
